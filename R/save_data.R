858eb8a3a20b53661eba760b1f9e4dbd1ce473ec
Usman Khaliq <usman.khaliq91@gmail.com>
2020-03-26 17:41:32 -0700

    updated recovered script

--- R/save_data.R
+++ R/save_data.R
@@ -7,7 +7,7 @@
 
 
 
-url_recovered <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+url_recovered <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
 
 url_deaths <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
